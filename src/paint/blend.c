62bb506128121276ddf0dacf0020bfe25bea48f3
Alex Kling <momiji.kling@gmail.com>
2025-03-25 11:41:40 +0100

    protected color overflow

--- src/paint/blend.c
+++ src/paint/blend.c
@@ -15,6 +15,22 @@ int	merge_rgb(t_color rgb)
 	return (encode_rgb(rgb.red, rgb.green, rgb.blue));
 }
 
+/* To avoid rgb overflow */
+t_byte	mix_color(t_byte color1, t_byte color2, float ratio)
+{
+	int		integer;
+	t_byte	result;
+
+	integer = color1 * (1 - ratio) + color2 * ratio;
+	if (integer >= 255)
+		result = 255;
+	else if (integer <= 0)
+		result = 0;
+	else
+		result = (t_byte)integer;
+	return (result);
+}
+
 int	blend(int color1, int color2, float ratio)
 {
 	t_color	c1;
@@ -23,9 +39,12 @@ int	blend(int color1, int color2, float ratio)
 
 	c1 = split_rgb(color1);
 	c2 = split_rgb(color2);
-	mix.red = c1.red * (1 - ratio) + c2.red * ratio;
-	mix.green = c1.green * (1 - ratio) + c2.green * ratio;
-	mix.blue = c1.blue * (1 - ratio) + c2.blue * ratio;
+	mix.red = mix_color(c1.red, c2.red, ratio);
+	mix.green = mix_color(c1.green, c2.green, ratio);
+	mix.blue = mix_color(c1.blue, c2.blue, ratio);
+	// mix.red = c1.red * (1 - ratio) + c2.red * ratio;
+	// mix.green = c1.green * (1 - ratio) + c2.green * ratio;
+	// mix.blue = c1.blue * (1 - ratio) + c2.blue * ratio;
 	return (merge_rgb(mix));
 }
 
--- src/raycasting/debug.c
+++ src/raycasting/debug.c
@@ -6,11 +6,12 @@ void	debug_print(t_cub *cub, int pixel_column)
 	{
 		(void)cub;
 		//printf("\n\npixel column is %d\n", pixel_column);
-		//printf("wall hit at X=[%d] Y=[%d]\n", cub->ray.x, cub->ray.y);
-		// printf("On side =[%d]\n", cub->ray.side);
-		// printf("dda_x =[%f]\n", cub->ray.dda_x);
-		// printf("dda_y =[%f]\n", cub->ray.dda_y);
-		// printf("Wall distance =[%f]\n", cub->ray.wall_dist);
+		printf("player at X=[%f] Y=[%f]\n", cub->player.grid_pt.x, cub->player.grid_pt.y);
+		printf("wall hit at X=[%d] Y=[%d]\n", cub->ray.x, cub->ray.y);
+		printf("On side =[%d]\n", cub->ray.side);
+		printf("dda_x =[%f]\n", cub->ray.dda_x);
+		printf("dda_y =[%f]\n", cub->ray.dda_y);
+		printf("Wall distance =[%f]\n", cub->ray.wall_dist);
 		// printf("angle deg =[%f]\n", cub->ray.angle_deg);
 		// printf("sin angle deg =[%f]\n", sin(cub->ray.angle_deg));
 		// printf("cos angle deg =[%f]\n", cos(cub->ray.angle_deg));
@@ -22,7 +23,7 @@ void	debug_print(t_cub *cub, int pixel_column)
 		// 	cub->ray.x, cub->ray.y, cub->ray.step_x, cub->ray.step_y);
 		// printf("first wall pix is %d and last is %d\n",
 		// 	cub->ray.top_wall, cub->ray.end_wall);
-		printf("pretest dist in text=[%f]\n", cub->ray.dist_in_text);
+		// printf("pretest dist in text=[%f]\n", cub->ray.dist_in_text);
 		// printf("floor dist in text=[%f]\n", cub->ray.dist_in_text - floor(cub->ray.dist_in_text));
 	}
 }
